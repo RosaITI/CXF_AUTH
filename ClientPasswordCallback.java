94c58f0b07cb0076730c7ef2ade5d9c6f20aa2d8
RosaITI <rtrapero85@gmail.com>
2015-04-18 11:31:57 +0200

    soa auth

---
+++ ClientPasswordCallback.java
@@ -0,0 +1,22 @@
+package es.iti.formacion.soap.client.callback;
+
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+public class ClientPasswordCallback implements CallbackHandler {
+
+	   @Override
+	   public void handle(Callback[] callbacks) throws IOException,
+	   UnsupportedCallbackException {
+	        WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
+
+	        if ("joe".equals(pc.getIdentifier())) {
+	            pc.setPassword("joespassword");
+	        } // else {...} - can add more users, access DB, etc.
+	   }
+	}
